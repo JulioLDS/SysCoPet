b3eb1a977b4e6081a9a92d9f4a377dfb2f220ec2
Rodrigo Marques de Córdova <ixorohoxi@gmail.com>
2026-07-02 11:59:28 -0300

    Alteração nas dependências do android para rodar o flutter, teste da API e DB em nuvem no mobile e sincronização do ADB via wi-fi

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,11 +2,6 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ApiConfig {
   static String get baseUrl {
-    // URL para web
-    if (kIsWeb) {
-      return 'https://api-4upet.onrender.com/api/v1';
-    }
-    // URL para emulador Android
-    return 'http://10.0.2.2:3000/api/v1';
+    return 'https://api-4upet.onrender.com/api/v1';
   }
 }
